de5b51550469e06df1679184dfe47814e9fdd50b
gafri putra <gafri.putra@gmail.com>
2022-07-20 18:51:14 +0700

    rpc read list blog with stream

--- blog/client/read.go
+++ blog/client/read.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"log"
 
 	pb "github.com/gafriputra/grpc-udemy/blog/proto"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 func readBlog(c pb.BlogServiceClient, id string) *pb.Blog {
@@ -17,4 +20,24 @@ func readBlog(c pb.BlogServiceClient, id string) *pb.Blog {
 
 	log.Printf("Got blog: %v", res)
 	return res
+}
+
+func listBlog(c pb.BlogServiceClient) {
+	stream, err := c.ListBlogs(context.Background(), &emptypb.Empty{})
+
+	if err != nil {
+		log.Fatalf("Error listing blog: %v", err)
+	}
+
+	for {
+		res, err := stream.Recv()
+
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatalf("Error reading blog: %v", err)
+		}
+
+		fmt.Println(res)
+	}
 }
--- blog/server/read.go
+++ blog/server/read.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	pb "github.com/gafriputra/grpc-udemy/blog/proto"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 func (s *Server) ReadBlog(ctx context.Context, in *pb.BlogId) (data *pb.Blog, err error) {
@@ -27,5 +29,34 @@ func (s *Server) ReadBlog(ctx context.Context, in *pb.BlogId) (data *pb.Blog, er
 	}
 
 	data = documentToBlog(blogData)
+	return
+}
+
+func (s *Server) ListBlogs(in *emptypb.Empty, stream pb.BlogService_ListBlogsServer) (err error) {
+	cur, err := collection.Find(context.Background(), primitive.D{})
+	if err != nil {
+		err = status.Errorf(codes.Internal, fmt.Sprintf("Internal error: %v", err))
+		return
+	}
+	defer cur.Close(context.Background())
+	for cur.Next(context.Background()) {
+		data := &BlogItem{}
+		err = cur.Decode(data)
+		if err != nil {
+			err = status.Errorf(codes.Internal, fmt.Sprintf("Error while decoding data from MongoDB: %v", err))
+			return
+		}
+
+		err = stream.SendMsg(documentToBlog(data))
+		if err != nil {
+			err = status.Errorf(codes.Internal, fmt.Sprintf("Error while send streaming: %v", err))
+			return
+		}
+	}
+
+	if err = cur.Err(); err != nil {
+		err = status.Errorf(codes.Internal, fmt.Sprintf("Internal error: %v", err))
+	}
+
 	return
 }
